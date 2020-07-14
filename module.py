bc600f5a84461a7c3ae2d2abc22f8890d47787a1
akshay <akshay.dr.11@gmail.com>
2020-07-14 16:29:12 +0200

    Updated both module.py and  partially model.py

--- module.py
+++ module.py
@@ -1,7 +1,189 @@
 from __future__ import division
+
 import tensorflow as tf
+from tensorflow.keras import layers
+import tensorflow_addons as tfa
+
 from ops import *
 from utils import *
+
+def generator_unet():
+  gf_dim = 64
+  output_c_dim = 3 
+  is_training = True
+
+  dropout_rate = 0.5 if is_training else 1.0
+  
+  inputs = tf.keras.layers.Input(shape=(256,512,3,))
+
+  e1 = tf.keras.layers.Conv2D(gf_dim, (3, 3), padding="same")(inputs)
+  e1 = tfa.layers.InstanceNormalization() (e1)
+  e1 = tf.keras.layers.LeakyReLU() (e1)
+
+  e2 = tf.keras.layers.Conv2D(gf_dim*2, (3, 3), padding="same")(e1)
+  e2 = tfa.layers.InstanceNormalization() (e2)
+  e2 = tf.keras.layers.LeakyReLU() (e2)
+
+  e3 = tf.keras.layers.Conv2D(gf_dim*4, (3, 3), padding="same")(e2)
+  e3 = tfa.layers.InstanceNormalization() (e3)
+  e3 = tf.keras.layers.LeakyReLU() (e3)
+
+  e4 = tf.keras.layers.Conv2D(gf_dim*8, (3, 3), padding="same")(e3)
+  e4 = tfa.layers.InstanceNormalization() (e4)
+  e4 = tf.keras.layers.LeakyReLU() (e4)
+
+  e5 = tf.keras.layers.Conv2D(gf_dim*8, (3, 3), padding="same")(e4)
+  e5 = tfa.layers.InstanceNormalization() (e5)
+  e5 = tf.keras.layers.LeakyReLU() (e5)
+
+  e6 = tf.keras.layers.Conv2D(gf_dim*8, (3, 3), padding="same")(e5)
+  e6 = tfa.layers.InstanceNormalization() (e6)
+  e6 = tf.keras.layers.LeakyReLU() (e6)
+
+  e7 = tf.keras.layers.Conv2D(gf_dim*8, (3, 3), padding="same")(e6)
+  e7 = tfa.layers.InstanceNormalization() (e7)
+  e7 = tf.keras.layers.LeakyReLU() (e7)
+
+  e8 = tf.keras.layers.Conv2D(gf_dim*8, (3, 3), padding="same")(e7)
+  e8 = tfa.layers.InstanceNormalization() (e8)
+  e8 = tf.keras.layers.Activation('relu')(e8)
+  
+  d1 = tf.keras.layers.Conv2DTranspose(gf_dim*8, (3, 3), padding="same") (e8)
+  d1 = tf.keras.layers.Dropout(dropout_rate) (d1)
+  d1 = tfa.layers.InstanceNormalization() (d1)
+  d1 = tf.keras.layers.add([d1, e7])
+
+  d2 = tf.keras.layers.Conv2DTranspose(gf_dim*8, (3, 3), padding="same") (d1)
+  d2 = tf.keras.layers.Dropout(dropout_rate) (d2)
+  d2 = tfa.layers.InstanceNormalization() (d2)
+  d2 = tf.keras.layers.add([d2, e6])
+
+  d3 = tf.keras.layers.Conv2DTranspose(gf_dim*8, (3, 3), padding="same") (d2)
+  d3 = tf.keras.layers.Dropout(dropout_rate) (d3)
+  d3 = tfa.layers.InstanceNormalization() (d3)
+  d3 = tf.keras.layers.add([d3, e5]) 
+  d3 = tf.keras.layers.Activation('relu')(d3)
+
+  d4 = tf.keras.layers.Conv2DTranspose(gf_dim*8, (3, 3), padding="same") (d3)
+  d4 = tfa.layers.InstanceNormalization() (d4)
+  d4 = tf.keras.layers.add([d4, e4]) 
+
+  d5 = tf.keras.layers.Conv2DTranspose(gf_dim*4, (3, 3), padding="same") (d4)
+  d5 = tfa.layers.InstanceNormalization() (d5)
+  d5 = tf.keras.layers.add([d5, e3]) 
+
+  d6 = tf.keras.layers.Conv2DTranspose(gf_dim*2, (3, 3), padding="same") (d5)
+  d6 = tfa.layers.InstanceNormalization() (d6)
+  d6 = tf.keras.layers.add([d6, e2]) 
+
+  d7 = tf.keras.layers.Conv2DTranspose(gf_dim, (3, 3), padding="same") (d6)
+  d7 = tfa.layers.InstanceNormalization() (d7)
+  d7 = tf.keras.layers.add([d7, e1]) 
+  d7 = tf.keras.layers.Activation('relu')(d7)
+
+  d8 = tf.keras.layers.Conv2DTranspose(output_c_dim, (3, 3), padding="same") (d7)
+
+  G_model = tf.keras.Model(inputs = inputs, outputs = d8)
+
+  return G_model
+
+def residule_block(x, dim, ks=3, s=1):
+  p = int((ks - 1) / 2)
+  y = tf.pad(x, [[0, 0], [p, p], [p, p], [0, 0]], "REFLECT")
+  y = tf.keras.layers.Conv2D(dim, (ks, ks), strides=(s,s), padding="valid")(y)
+  y = tfa.layers.InstanceNormalization() (y)
+  y =  tf.keras.layers.Activation('relu') (y)
+  y = tf.pad(y, [[0, 0], [p, p], [p, p], [0, 0]], "REFLECT")
+  y = tf.keras.layers.Conv2D(dim, (ks, ks), strides=(s,s), padding="valid")(y)
+  y = tfa.layers.InstanceNormalization() (y)
+  return y + x
+
+def generator_resnet():
+  gf_dim = 64
+  output_c_dim = 3 
+  
+  inputs = tf.keras.layers.Input(shape=(256,512,3,))
+  
+  # Justin Johnson's model from https://github.com/jcjohnson/fast-neural-style/
+  # The network with 9 blocks consists of: c7s1-32, d64, d128, R128, R128, R128,
+  # R128, R128, R128, R128, R128, R128, u64, u32, c7s1-3
+  c0 = tf.pad(inputs, [[0, 0], [3, 3], [3, 3], [0, 0]], "REFLECT")
+
+  c1 = tf.keras.layers.Conv2D(gf_dim, (7, 7), strides=(1,1), padding="valid")(c0)
+  c1 = tfa.layers.InstanceNormalization() (c1)
+  c1 =  tf.keras.layers.Activation('relu') (c1)
+
+  c2 = tf.keras.layers.Conv2D(gf_dim*2, (3, 3), strides=(2,2), padding="same")(c1)
+  c2 = tfa.layers.InstanceNormalization() (c2)
+  c2 =  tf.keras.layers.Activation('relu') (c2)
+
+  c3 = tf.keras.layers.Conv2D(gf_dim*4, (3, 3), strides=(2,2), padding="same")(c2)
+  c3 = tfa.layers.InstanceNormalization() (c3)
+  c3 =  tf.keras.layers.Activation('relu') (c3)
+
+  r1 = residule_block(c3, gf_dim*4)
+  r2 = residule_block(r1, gf_dim*4)
+  r3 = residule_block(r2, gf_dim*4)
+  r4 = residule_block(r3, gf_dim*4)
+  r5 = residule_block(r4, gf_dim*4)
+  r6 = residule_block(r5, gf_dim*4)
+  r7 = residule_block(r6, gf_dim*4)
+  r8 = residule_block(r7, gf_dim*4)
+  r9 = residule_block(r8, gf_dim*4)
+
+  d1 = tf.keras.layers.Conv2DTranspose(gf_dim*2, (3, 3), strides=(2,2), padding="same") (r9)
+  d1 = tfa.layers.InstanceNormalization() (d1)
+  d1 =  tf.keras.layers.Activation('relu') (d1)
+
+  d2 = tf.keras.layers.Conv2DTranspose(gf_dim, (3, 3), strides=(2,2), padding="same") (d1)
+  d2 = tfa.layers.InstanceNormalization() (d2)
+  d2 =  tf.keras.layers.Activation('relu') (d2)
+
+  d2 = tf.pad(d2, [[0, 0], [3, 3], [3, 3], [0, 0]], "REFLECT")
+
+  d2 = tf.keras.layers.Conv2D(output_c_dim, (7, 7), strides=(1,1), padding="valid")(d2)
+  pred =  tf.keras.layers.Activation('tanh') (d2)
+
+  G_model = tf.keras.Model(inputs = inputs, outputs = pred)
+
+  return G_model
+
+
+def discriminator():
+  df_dim = 64
+  segment_class = 8
+  image_height = 256
+  image_width = 512
+
+  inputs = tf.keras.layers.Input(shape=(image_height,image_width,3,))
+  # mask = tf.keras.layers.Input(shape=(image_height,image_width,3,))
+  mask = tf.keras.layers.Input(shape=(32, 64, segment_class), dtype=tf.dtypes.float32)
+
+  h0 = tf.keras.layers.Conv2D(df_dim, (3, 3), strides=(2,2), padding="same")(inputs)
+  h0 = tf.keras.layers.LeakyReLU() (h0)
+
+  h1 = tf.keras.layers.Conv2D(df_dim*2, (3, 3), strides=(2,2), padding="same")(h0)
+  h1 = tfa.layers.InstanceNormalization() (h1)
+  h1 = tf.keras.layers.LeakyReLU() (h1)
+
+  h2 = tf.keras.layers.Conv2D(df_dim*4, (3, 3), strides=(2,2), padding="same")(h1)
+  h2 = tfa.layers.InstanceNormalization() (h2)
+  h2 = tf.keras.layers.LeakyReLU() (h2)
+
+  h3 = tf.keras.layers.Conv2D(df_dim*8, (3, 3), strides=(1,1), padding="same")(h2)
+  h3 = tfa.layers.InstanceNormalization() (h3)
+  h3 = tf.keras.layers.LeakyReLU() (h3)
+
+  h4 = tf.keras.layers.Conv2D(segment_class, (3, 3), padding="same")(h3)
+  h4 = tf.keras.layers.multiply([h4, mask])
+
+  h4_mask = tf.math.reduce_sum(h4, axis=-1, keepdims=True)
+  
+  D_model = tf.keras.Model(inputs = [inputs, mask], outputs = h4_mask)
+
+  return D_model
+
+# Other functions 
 
 def tf_kernel_prep_3d(kernel, n_channels):
     return np.tile(kernel, (n_channels, 1, 1)).swapaxes(0,1).swapaxes(1,2)
@@ -17,132 +199,18 @@ def tf_deriv(batch, ksize=3, padding='SAME'):
     kernel = tf.constant(np.stack([gx, gy], axis=-1), name="DerivKernel_image", dtype = np.float32)
     return tf.nn.depthwise_conv2d(batch, kernel, [1, 1, 1, 1], padding=padding, name="GradXY")
 
-def discriminator(image, mask, options, reuse=False, name="discriminator"):
-
-    with tf.variable_scope(name):
-        # image is 256 x 512 x input_c_dim
-        if reuse:
-            tf.get_variable_scope().reuse_variables()
-        else:
-            assert tf.get_variable_scope().reuse is False
-
-        h0 = lrelu(conv2d(image, options.df_dim, name='d_h0_conv'))
-        # h0 is (128 x 256 x self.df_dim)
-        h1 = lrelu(instance_norm(conv2d(h0, options.df_dim*2, name='d_h1_conv'), 'd_bn1'))
-        # h1 is (64 x 128 x self.df_dim*2)
-        h2 = lrelu(instance_norm(conv2d(h1, options.df_dim*4, name='d_h2_conv'), 'd_bn2'))
-        # h2 is (32x 64 x self.df_dim*4)
-        h3 = lrelu(instance_norm(conv2d(h2, options.df_dim*8, s=1, name='d_h3_conv'), 'd_bn3'))
-        # h3 is (32 x 64 x self.df_dim*8)
-        h4 = conv2d(h3, options.segment_class, s=1, name='d_h4_conv')
-        h4_mask = tf.reduce_sum(tf.multiply(h4, mask), axis=-1, keep_dims=True)
-        # h4_mask is (32 x 64 x 1)
-        return h4_mask
-
-
-def generator_unet(image, options, reuse=False, name="generator"):
-
-    dropout_rate = 0.5 if options.is_training else 1.0
-    with tf.variable_scope(name):
-        if reuse:
-            tf.get_variable_scope().reuse_variables()
-        else:
-            assert tf.get_variable_scope().reuse is False
-
-        e1 = instance_norm(conv2d(image, options.gf_dim, name='g_e1_conv'))
-        e2 = instance_norm(conv2d(lrelu(e1), options.gf_dim*2, name='g_e2_conv'), 'g_bn_e2')
-        e3 = instance_norm(conv2d(lrelu(e2), options.gf_dim*4, name='g_e3_conv'), 'g_bn_e3')
-        e4 = instance_norm(conv2d(lrelu(e3), options.gf_dim*8, name='g_e4_conv'), 'g_bn_e4')
-        e5 = instance_norm(conv2d(lrelu(e4), options.gf_dim*8, name='g_e5_conv'), 'g_bn_e5')
-        e6 = instance_norm(conv2d(lrelu(e5), options.gf_dim*8, name='g_e6_conv'), 'g_bn_e6')
-        e7 = instance_norm(conv2d(lrelu(e6), options.gf_dim*8, name='g_e7_conv'), 'g_bn_e7')
-        e8 = instance_norm(conv2d(lrelu(e7), options.gf_dim*8, name='g_e8_conv'), 'g_bn_e8')
-
-        d1 = deconv2d(tf.nn.relu(e8), options.gf_dim*8, name='g_d1')
-        d1 = tf.nn.dropout(d1, dropout_rate)
-        d1 = tf.concat([instance_norm(d1, 'g_bn_d1'), e7], 3)
-
-        d2 = deconv2d(tf.nn.relu(d1), options.gf_dim*8, name='g_d2')
-        d2 = tf.nn.dropout(d2, dropout_rate)
-        d2 = tf.concat([instance_norm(d2, 'g_bn_d2'), e6], 3)
-
-        d3 = deconv2d(tf.nn.relu(d2), options.gf_dim*8, name='g_d3')
-        d3 = tf.nn.dropout(d3, dropout_rate)
-        d3 = tf.concat([instance_norm(d3, 'g_bn_d3'), e5], 3)
-
-        d4 = deconv2d(tf.nn.relu(d3), options.gf_dim*8, name='g_d4')
-        d4 = tf.concat([instance_norm(d4, 'g_bn_d4'), e4], 3)
-
-        d5 = deconv2d(tf.nn.relu(d4), options.gf_dim*4, name='g_d5')
-        d5 = tf.concat([instance_norm(d5, 'g_bn_d5'), e3], 3)
-
-        d6 = deconv2d(tf.nn.relu(d5), options.gf_dim*2, name='g_d6')
-        d6 = tf.concat([instance_norm(d6, 'g_bn_d6'), e2], 3)
-
-        d7 = deconv2d(tf.nn.relu(d6), options.gf_dim, name='g_d7')
-        d7 = tf.concat([instance_norm(d7, 'g_bn_d7'), e1], 3)
-
-        d8 = deconv2d(tf.nn.relu(d7), options.output_c_dim, name='g_d8')
-
-        return tf.nn.tanh(d8)
-
-
-def generator_resnet(image, options, reuse=False, name="generator"):
-
-    with tf.variable_scope(name):
-        if reuse:
-            tf.get_variable_scope().reuse_variables()
-        else:
-            assert tf.get_variable_scope().reuse is False
-
-        def residule_block(x, dim, ks=3, s=1, name='res'):
-            p = int((ks - 1) / 2)
-            y = tf.pad(x, [[0, 0], [p, p], [p, p], [0, 0]], "REFLECT")
-            y = instance_norm(conv2d(y, dim, ks, s, padding='VALID', name=name+'_c1'), name+'_bn1')
-            y = tf.pad(tf.nn.relu(y), [[0, 0], [p, p], [p, p], [0, 0]], "REFLECT")
-            y = instance_norm(conv2d(y, dim, ks, s, padding='VALID', name=name+'_c2'), name+'_bn2')
-            return y + x
-
-        # Justin Johnson's model from https://github.com/jcjohnson/fast-neural-style/
-        # The network with 9 blocks consists of: c7s1-32, d64, d128, R128, R128, R128,
-        # R128, R128, R128, R128, R128, R128, u64, u32, c7s1-3
-        c0 = tf.pad(image, [[0, 0], [3, 3], [3, 3], [0, 0]], "REFLECT")
-        c1 = tf.nn.relu(instance_norm(conv2d(c0, options.gf_dim, 7, 1, padding='VALID', name='g_e1_c'), 'g_e1_bn'))
-        c2 = tf.nn.relu(instance_norm(conv2d(c1, options.gf_dim*2, 3, 2, name='g_e2_c'), 'g_e2_bn'))
-        c3 = tf.nn.relu(instance_norm(conv2d(c2, options.gf_dim*4, 3, 2, name='g_e3_c'), 'g_e3_bn'))
-        # define G network with 9 resnet blocks
-        r1 = residule_block(c3, options.gf_dim*4, name='g_r1')
-        r2 = residule_block(r1, options.gf_dim*4, name='g_r2')
-        r3 = residule_block(r2, options.gf_dim*4, name='g_r3')
-        r4 = residule_block(r3, options.gf_dim*4, name='g_r4')
-        r5 = residule_block(r4, options.gf_dim*4, name='g_r5')
-        r6 = residule_block(r5, options.gf_dim*4, name='g_r6')
-        r7 = residule_block(r6, options.gf_dim*4, name='g_r7')
-        r8 = residule_block(r7, options.gf_dim*4, name='g_r8')
-        r9 = residule_block(r8, options.gf_dim*4, name='g_r9')
-
-        d1 = deconv2d(r9, options.gf_dim*2, 3, 2, name='g_d1_dc')
-        d1 = tf.nn.relu(instance_norm(d1, 'g_d1_bn'))
-        d2 = deconv2d(d1, options.gf_dim, 3, 2, name='g_d2_dc')
-        d2 = tf.nn.relu(instance_norm(d2, 'g_d2_bn'))
-        d2 = tf.pad(d2, [[0, 0], [3, 3], [3, 3], [0, 0]], "REFLECT")
-        pred = tf.nn.tanh(conv2d(d2, options.output_c_dim, 7, 1, padding='VALID', name='g_pred_c'))
-
-        return pred
-
-
 def abs_criterion(in_, target):
-    return tf.reduce_mean(tf.abs(in_ - target))
+    return tf.math.reduce_mean(tf.abs(in_ - target))
 
 
 def mae_criterion(in_, target):
-    return tf.reduce_mean((in_-target)**2)
+    return tf.math.reduce_mean((in_-target)**2)
 
 
 def sce_criterion(logits, labels):
-    return tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=labels))
+    return tf.math.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=labels))
 
 def gradloss_criterion(in_, target, weight):
     abs_deriv = tf.abs(tf.abs(tf_deriv(in_)) - tf.abs(tf_deriv(target)))
-    abs_deriv = tf.reduce_mean(abs_deriv, axis=-1, keep_dims=True)
-    return tf.reduce_mean(tf.multiply(weight, abs_deriv))
+    abs_deriv = tf.math.reduce_mean(abs_deriv, axis=-1, keep_dims=True)
+    return tf.math.reduce_mean(tf.multiply(weight, abs_deriv))
